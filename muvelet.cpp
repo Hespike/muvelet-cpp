fceef4918ff5f976eed0248709f0668a3ff2dd6c
Sárdi András <sardi.andras@gmail.com>
2021-10-12 08:59:30 +0200

    Create muvelet.cpp

---
+++ muvelet.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int szam = 0;
+    int muvelet= 0;
+    cin >> szam;
+
+    muvelet = szam * szam - 2 * szam;
+    cout << muvelet;
+
+    return 0;
+}
+
+/*
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int szam;
+    cin >> szam;
+    cout << (szam * szam) - (2 * szam);
+    return 0;
+}
+ */
